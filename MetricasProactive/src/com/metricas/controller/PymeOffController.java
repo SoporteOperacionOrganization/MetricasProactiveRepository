3ac0c90d6cbcf8ad2fd2569c82e3501e184a7194
ls77996 <ls77996@LACMX00EBAS0308.lac.nsroot.net>
2016-12-05 11:36:24 -0600

    controladores

---
+++ MetricasProactive/src/com/metricas/controller/PymeOffController.java
@@ -0,0 +1,31 @@
+package com.metricas.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import com.metricas.custommodel.CustomUsuario;
+import com.metricas.service.UsuarioService;
+
+
+@Controller
+public class PymeOffController {
+
+	@Autowired
+	private UsuarioService usuarioService;
+	
+	@RequestMapping(value = "/pymeOffline", method = RequestMethod.GET)
+	public String test(Model model){
+		CustomUsuario principal = (CustomUsuario) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		String soeidSesion = principal.getNombre();
+		model.addAttribute("soeidSesion", soeidSesion);
+		
+		int valores[] ={1,2,3,4,5};
+		
+		//usuarios = usuarioService.obtenerUsuarios();
+		//model.addAttribute("usuarios", usuarios);
+		return "login/pymeOffline";
+	}
+	
+}
