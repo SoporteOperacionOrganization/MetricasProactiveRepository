acefac11610b26d0f649ffc231c2f877450b81fe
ap77990 <ap77990@LACMX00EBAS0311.lac.nsroot.net>
2016-12-14 17:45:41 -0600

    Arreglo de detalle en dataset de graficas familias y segmentos

--- MetricasProactive/src/com/metricas/service/SegmentoServiceImpl.java
+++ MetricasProactive/src/com/metricas/service/SegmentoServiceImpl.java
@@ -97,6 +97,19 @@ public class SegmentoServiceImpl implements SegmentoService {
 						}
 					}
 				}
+				
+				for (Map.Entry<String, Integer> entryC : totalesLlamadasComparativo.entrySet()) {
+					String llaveC = entryC.getKey();
+					Integer valorC = entryC.getValue();
+					Integer busqueda = totalesLlamadasOriginal.get(llaveC);
+					if(busqueda == null){
+						List<Integer> totales2 = new ArrayList<Integer>();
+						totales2.add(0);
+						totales2.add(valorC);
+						listadoTotales.put(llaveC, totales2);
+					}
+				}
+				
 			}
 
 		} else {
@@ -141,6 +154,20 @@ public class SegmentoServiceImpl implements SegmentoService {
 					}
 				}
 			}
+			
+			
+			for (Map.Entry<String, Integer> entryO : totalesLlamadasOriginal.entrySet()) {
+				String llaveO = entryO.getKey();
+				Integer valorO = entryO.getValue();
+				Integer busqueda = totalesLlamadasComparativo.get(llaveO);
+				if(busqueda == null){
+					List<Integer> totales2 = new ArrayList<Integer>();
+					totales2.add(valorO);
+					totales2.add(0);
+					listadoTotales.put(llaveO, totales2);
+				}
+			}
+			
 
 		}
 		String json = gson.toJson(listadoTotales);
@@ -210,6 +237,21 @@ public class SegmentoServiceImpl implements SegmentoService {
 						}
 					}
 				}
+				
+				for (Map.Entry<String, Integer> entryC : totalesLlamadasComparativo.entrySet()) {
+					String llaveC = entryC.getKey();
+					Integer valorC = entryC.getValue();
+					Integer busqueda = totalesLlamadasOriginal.get(llaveC);
+					if(busqueda == null){
+						List<Integer> totales2 = new ArrayList<Integer>();
+						totales2.add(0);
+						totales2.add(valorC);
+						listadoTotales.put(llaveC, totales2);
+					}
+					
+				}
+				System.out.println("MAYOR ORIGINAL " + listadoTotales);
+				
 			}
 		} else {
 			if (tamanoOriginal == 0) {
@@ -253,6 +295,20 @@ public class SegmentoServiceImpl implements SegmentoService {
 					}
 				}
 			}
+			
+			
+			for (Map.Entry<String, Integer> entryO : totalesLlamadasOriginal.entrySet()) {
+				String llaveO = entryO.getKey();
+				Integer valorO = entryO.getValue();
+				Integer busqueda = totalesLlamadasComparativo.get(llaveO);
+				if(busqueda == null){
+					List<Integer> totales2 = new ArrayList<Integer>();
+					totales2.add(valorO);
+					totales2.add(0);
+					listadoTotales.put(llaveO, totales2);
+				}
+			}
+			
 		}
 
 		String json = gson.toJson(listadoTotales);
